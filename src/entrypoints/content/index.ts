5e052b509feee040edebcb17f63761dfe2447c50
Fio <fuyitao2018@gmail.com>
2025-07-27 21:09:45 +0800

    init

---
+++ src/entrypoints/content/index.ts
@@ -0,0 +1,26 @@
+import "./style.css";
+import { createApp } from "vue";
+import App from "./App.vue";
+
+export default defineContentScript({
+  matches: ["<all_urls>"],
+  cssInjectionMode: "ui",
+
+  async main(ctx) {
+    const ui = await createShadowRootUi(ctx, {
+      name: "layer-craft-editor-panel",
+      position: "inline",
+      anchor: "body",
+      onMount: (container) => {
+        const app = createApp(App);
+        app.mount(container);
+        return app;
+      },
+      onRemove: (app) => {
+        app?.unmount();
+      },
+    });
+
+    ui.mount();
+  },
+});
