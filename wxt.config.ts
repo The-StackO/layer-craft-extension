2588e534dab1d80dcf7a14aa0969e84c83dfaf2c
Fio <fuyitao2018@gmail.com>
2025-07-30 15:03:40 +0800

    feat: 调整描述文案

--- wxt.config.ts
+++ wxt.config.ts
@@ -9,8 +9,9 @@ export default defineConfig({
     plugins: [tailwindcss()],
   }),
   manifest: {
-    name: '层舟-网页修改',
-    description: '层舟-网页修改',
+    name: '层舟-网页魔术贴',
+    description:
+      '层舟(LayerCraft) 像用魔术贴一样，轻松修改网页文本、替换图片，支持一键撤销和截图分享。是一款不会编程也能轻松上手的网页编辑器。',
     permissions: ['storage'],
   },
 });
